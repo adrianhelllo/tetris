9ae76063cb6031bfccea17db1b0b7fa4b92fca22
adrianhelllo <hetessyadrian@gmail.com>
2025-07-21 13:12:31 +0200

    falling cells

--- logic.py
+++ logic.py
@@ -1,6 +1,21 @@
 import time
+import copy
 
 def update_board(board):
     board.clear_previous()
-    time.sleep(0.25)
+    time.sleep(0.05)
     board.render_board()
+
+def shift_board(board):
+    shifted_board = copy.deepcopy(board)
+
+    for y in range(len(shifted_board) - 2, -1, -1):
+        for x in range(len(shifted_board[0])):
+            if shifted_board[y][x] == 1 and shifted_board[y + 1][x] ==  0:
+                shifted_board[y][x], shifted_board[y + 1][x] = 0, 1
+
+    return shifted_board
+
+def check_collision():
+    pass
+
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import time
 import board as board_f
 import logic as logic_f
 import tetromino as piece_f
 import config as config_f
+from logic import update_board as update
+from logic import shift_board as shift
+
 
 def main():
     playing = True
     board_obj = board_f.Board()
     piece = piece_f.Tetromino()
     board_obj.board = piece.spawn_tetromino(board_obj.board)
-    logic_f.update_board(board_obj)
+    update(board_obj)
+
+    for i in range(20):
+        time.sleep(0.25)
+        board_obj.board = shift(board_obj.board)
+        update(board_obj)
+
+        if i % 4 == 0:
+            print("creating new tetromino")
+            piece = piece_f.Tetromino()
 
 if __name__ == '__main__':
     main()
